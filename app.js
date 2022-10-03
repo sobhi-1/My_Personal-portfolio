cbfcadb3062219f5536b31f99770dd82aacf0bfc
Sobhi Mohamed <113847982+sobhi-1@users.noreply.github.com>
2022-10-03 06:28:54 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontints = document.getElementsByClassName("tab-contents");
+
+function ope(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontint of tabcontints) {
+    tabcontint.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
